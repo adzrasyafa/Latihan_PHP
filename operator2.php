95abccdea555fb72451cc454474c34cf7b2bd058
adzrasyafa <e42241974@student.polije.ac.id>
2025-09-23 15:22:16 +0700

    latihan php

---
+++ operator2.php
@@ -0,0 +1,25 @@
+<?php 
+$x=10; 
+$y=6; 
+echo ($x + $y); 
+echo "<br />";
+
+echo ($x - $y); 
+echo "<br />";
+
+echo ($x * $y); 
+echo "<br />";
+
+echo ($x / $y); 
+echo "<br />";
+
+echo ($x % $y); 
+echo "<br />";
+
+$a = "Hello"; 
+$b = $a . " world!"; 
+echo $b; 
+echo "<br />";
+
+Echo $a. "--"   . $b. "ini string operator" ;
+?>
---
+++ variabel.php
@@ -0,0 +1,15 @@
+<?php
+$nim="E42241974";
+$nama="Adzra Syafa Kamila";
+$prodi="Bisnis Digital";
+echo "Selamat Praktikum Wrokshop Sistem Terintegrasi";
+echo "<br \>";
+
+echo $nim;
+echo "<br \>";
+
+echo $nama;
+echo "<br \>";
+
+echo $prodi;
+?>
